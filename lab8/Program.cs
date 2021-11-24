4523e9ea9f7674a897ce2abe2ac08c1424c9229e
Дмитрий Леонов <dleonov701@gmail.com>
2021-11-24 18:29:38 +0300

    Добавьте файлы проекта.

---
+++ lab8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public interface ICommonInterface<T>
+    {
+        T Add();
+        T Delete();
+        T Get();
+    }
+}
